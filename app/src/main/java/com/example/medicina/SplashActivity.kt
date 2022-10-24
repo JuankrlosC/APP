07218a0821ec4fb05fa332b395e130e4be493e3d
juan Carlos <juankrlos27.jccn@gmail.com>
2022-10-23 21:25:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/medicina/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.medicina
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.medicina.databinding.ActivitySplashBinding
+
+
+class SplashActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        }
+
+    override fun onStart() {
+        super.onStart()
+        binding.animation.playAnimation()
+
+    }
+    }
+
